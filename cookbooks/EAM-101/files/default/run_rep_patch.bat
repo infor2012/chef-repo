27f629a58d35626db05108ec850ef375a31a7759
Infor <markandeyulu.nagulavancha@infor.com>
2012-08-29 15:56:16 +0530

    changed bat files
    removed pause command

--- cookbooks/EAM-101/files/default/run_rep_patch.bat
+++ cookbooks/EAM-101/files/default/run_rep_patch.bat
@@ -1,3 +1,2 @@
 @echo off
 start c:\python27\python C:\wamp\www\installAll\patch\Patchinstall_adv.py C:\\inforEAMApp C:\\Windows\\cwi\\infor_Patches\\extractFolder EAM123 inhyvwamssyteln sa Ssa0n!y callrep
-PAUSE 
