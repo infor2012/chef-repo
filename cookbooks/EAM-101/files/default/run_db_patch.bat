54810ca9752a21fcfbf6110e59af476614797f87
Infor <markandeyulu.nagulavancha@infor.com>
2012-08-30 17:47:34 +0530

    change batch script EAM101 added

--- cookbooks/EAM-101/files/default/run_db_patch.bat
+++ cookbooks/EAM-101/files/default/run_db_patch.bat
@@ -1,2 +1,2 @@
 @echo off
-start c:\python27\python C:\wamp\www\installAll\patch\Patchinstall_adv.py C:\\inforEAMApp C:\\Windows\\cwi\\infor_Patches\\ EAM123 inhyvwamssyteln sa Ssa0n!y calldb
+start c:\python27\python C:\wamp\www\installAll\patch\Patchinstall_adv.py C:\\inforEAMApp C:\\Windows\\cwi\\infor_Patches\\ EAM123 inhyvwamssyteln sa Ssa0n!y db EAM101
