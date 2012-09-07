dcadb5333328cf978d81abf48d5f8c407aa91ff0
Infor <markandeyulu.nagulavancha@infor.com>
2012-09-07 12:44:02 +0530

    changed the batch files

--- cookbooks/EAM-101/files/default/run_app_patch.bat
+++ cookbooks/EAM-101/files/default/run_app_patch.bat
@@ -1,3 +1,3 @@
 @echo off
 cd \
-start c:\python27\python C:\wamp\www\installAll\patch\Patchinstall_adv.py C:\\inforEAMApp C:\\Windows\\cwi\\infor_Patches\\ EAM123 inhyvwamssyteln sa Ssa0n!y app EAM101
+start c:\python27\python C:\wamp\www\installAll\patch\Patchinstall_adv.py app
